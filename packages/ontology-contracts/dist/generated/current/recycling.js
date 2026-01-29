869d343c72473985d50406e97e7b83277da333c9
Aleix Soler <aleixsoler@blueroominnovation.com>
2026-01-29 15:24:22 +0100

    chore: builded with fixes

--- packages/ontology-contracts/dist/generated/current/recycling.js
+++ packages/ontology-contracts/dist/generated/current/recycling.js
@@ -1,7 +1,7 @@
 /**
  * Auto-generated TypeScript definitions from JSON Schema
  * DO NOT EDIT MANUALLY
- * Generated: 2026-01-27 13:47:30
+ * Generated: 2026-01-29 14:21:56
  * Source: shapes/v0.1/recycling.shacl.ttl
  */
 export {};
