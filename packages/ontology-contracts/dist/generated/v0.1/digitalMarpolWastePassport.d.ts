e75f64372b0092c4b3666b347bda6d19bb4e3ec8
Aleix Soler <aleixsoler@blueroominnovation.com>
2026-01-30 09:32:13 +0100

    chore: build update generated TypeScript definitions and documentation for v0.1

--- packages/ontology-contracts/dist/generated/v0.1/digitalMarpolWastePassport.d.ts
+++ packages/ontology-contracts/dist/generated/v0.1/digitalMarpolWastePassport.d.ts
@@ -1,7 +1,7 @@
 /**
  * Auto-generated TypeScript definitions from JSON Schema
  * DO NOT EDIT MANUALLY
- * Generated: 2026-01-29 14:21:54
+ * Generated: 2026-01-30 07:56:06
  * Source: shapes/v0.1/digitalMarpolWastePassportShapes.ttl
  */
 /**
